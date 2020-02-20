a327948c5ad26b341297c74e6b437be70b32d769
Michael Yearsley <59950440+michaelyearsley@users.noreply.github.com>
2020-02-19 22:27:41 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ArrayPrat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(reverse());
+            Console.WriteLine(code("Hello"));
+            Console.WriteLine(angle(27, 59));
+            Console.WriteLine(angle(45, 45));
+            Console.WriteLine(angle(150, 30));
+            Console.WriteLine(MyPi(5));
+            Console.WriteLine(MyPi(4));
+            Console.WriteLine(MyPi(15));
+        }
+        static string reverse()
+        {
+            string x = Convert.ToString(150);
+            int l = x.Length;
+            StringBuilder reverse = new StringBuilder("", l); ;
+            char z = 'f';
+            l = x.Length - 1;
+            
+            for (int i = x.Length - 1; i >= 0; i--)
+            {
+                z = Convert.ToChar(x[i]);
+                reverse.Append(z);
+            }
+            return Convert.ToString(reverse);
+        }
+        static string code(string word)
+        {
+
+            word = word.ToLower(); 
+            string result = "";
+
+            Dictionary<char, int> duplicate = new Dictionary<char, int>();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (duplicate.ContainsKey(word[i])) 
+                    duplicate[word[i]]++;
+                else 
+                    duplicate.Add(word[i], 1);
+            }
+            foreach (char cha in word)
+            {
+                if (duplicate[cha] == 1) 
+                    result = result + "(";
+                else 
+                   result += ")";
+            }
+            return result;
+        }
+        static string angle(int A1, int A2)
+        {
+            string A3 = "";
+            if ((A1 + A2) == 90)
+                A3 = "right";
+            else if(A1 +A2 < 90)
+                A3 = "acute";
+            else
+                A3 = "obtuse";
+            return A3;
+        }
+        static double MyPi(int p1)
+        {
+            double pi = Math.Round(Math.PI, p1);
+            return pi;
+        }
+    }
+    
+}
